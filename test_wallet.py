fc42078315125ba9367e50c95420a0a39b5de036
Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>
2018-08-15 13:24:42 +0300

    + IS-883 update

--- test_wallet.py
+++ test_wallet.py
@@ -1,4 +1,7 @@
-from utils import *
+from indy import pool, wallet
+import random
+import string
+import json
 import time
 import pytest
 
@@ -6,24 +9,32 @@ import pytest
 @pytest.mark.asyncio
 async def test_key_derivation_algorithm():
     await pool.set_protocol_version(2)
-    t1 = float(time.time())
-    await wallet_helper()
-    t2 = float(time.time())
-    await wallet_helper(wallet_key_derivation_method='ARAGON2I_INT')
-    t3 = float(time.time())
-    assert((t3 - t2) < (t2 - t1))
-    print('ARAGON2I_MOD: %f' % (t2 - t1))
-    print('ARAGON2I_INT: %f' % (t3 - t2))
+    config_mod = json.dumps({"id": ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))})
+    credentials_mod = json.dumps({"key": '', "key_derivation_method": 'ARAGON2I_MOD'})
+    t1_start = float(time.time())
+    await wallet.create_wallet(config_mod, credentials_mod)
+    t1_create_delta = float(time.time()) - t1_start
+    handle_mod = await wallet.open_wallet(config_mod, credentials_mod)
+    t1_open_delta = float(time.time()) - t1_start - t1_create_delta
+    await wallet.close_wallet(handle_mod)
+    t1_close_delta = float(time.time()) - t1_start - t1_create_delta - t1_open_delta
+    await wallet.delete_wallet(config_mod, credentials_mod)
+    t1_delete_delta = float(time.time()) - t1_start - t1_create_delta - t1_open_delta - t1_close_delta
+    print('\n', t1_create_delta, '\n', t1_open_delta, '\n', t1_close_delta, '\n', t1_delete_delta)
 
+    config_int = json.dumps({"id": ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))})
+    credentials_int = json.dumps({"key": '', "key_derivation_method": 'ARAGON2I_INT'})
+    t2_start = float(time.time())
+    await wallet.create_wallet(config_int, credentials_int)
+    t2_create_delta = float(time.time()) - t2_start
+    handle_int = await wallet.open_wallet(config_int, credentials_int)
+    t2_open_delta = float(time.time()) - t2_start - t2_create_delta
+    await wallet.close_wallet(handle_int)
+    t2_close_delta = float(time.time()) - t2_start - t2_create_delta - t2_open_delta
+    await wallet.delete_wallet(config_int, credentials_int)
+    t2_delete_delta = float(time.time()) - t2_start - t2_create_delta - t2_open_delta - t2_close_delta
+    print('\n', t2_create_delta, '\n', t2_open_delta, '\n', t2_close_delta, '\n', t2_delete_delta)
 
-@pytest.mark.asyncio
-async def test_key_derivation_algorithm_vice_versa():
-    await pool.set_protocol_version(2)
-    t1 = float(time.time())
-    await wallet_helper(wallet_key_derivation_method='ARAGON2I_INT')
-    t2 = float(time.time())
-    await wallet_helper()
-    t3 = float(time.time())
-    assert ((t3 - t2) > (t2 - t1))
-    print('ARAGON2I_INT: %f' % (t2 - t1))
-    print('ARAGON2I_MOD: %f' % (t3 - t2))
+    assert(t2_create_delta < t1_create_delta)
+    assert(t2_open_delta < t1_open_delta)
+    assert(t2_delete_delta < t1_delete_delta)

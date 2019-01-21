ea12b4596fc3b8066114c6d7a377bb56c338821e
Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>
2019-01-21 18:40:44 +0300

    update + fixtures

--- test_vc.py
+++ test_vc.py
@@ -6,20 +6,18 @@ from indy import did, ledger
 
 
 @pytest.mark.asyncio
-async def test_vc_by_restart():
-    await pool.set_protocol_version(2)
-    pool_handle, _ = await pool_helper()
-    wallet_handle, _, _ = await wallet_helper()
+async def test_vc_by_restart(looper, simple):
+    pool_handle, wallet_handle = simple
     trustee_did, trustee_vk = await did.create_and_store_my_did(wallet_handle, json.dumps(
         {'seed': '000000000000000000000000Trustee1'}))
     random_did = random_did_and_json()[0]
     another_random_did = random_did_and_json()[0]
 
     add_before = await nym_helper(pool_handle, wallet_handle, trustee_did, random_did)
-    time.sleep(1)
+    assert add_before['op'] == 'REPLY'
+    time.sleep(2)
     get_before = await get_nym_helper(pool_handle, wallet_handle, trustee_did, random_did)
-
-    print(add_before, '\n', get_before)
+    assert get_before['result']['seqNo'] is not None
 
     req = await ledger.build_get_validator_info_request(trustee_did)
     results = json.loads(await ledger.sign_and_submit_request(pool_handle, wallet_handle, trustee_did, req))
@@ -27,29 +25,25 @@ async def test_vc_by_restart():
     primary_before =\
         result['result']['data']['Node_info']['Replicas_status']['Node4:0']['Primary'][len('Node'):-len(':0')]
     host = testinfra.get_host('docker://node'+primary_before)
-    cmd = host.run('systemctl stop indy-node')
-    print(cmd)
+    host.run('systemctl stop indy-node')
 
     time.sleep(120)
-    cmd = host.run('systemctl start indy-node')
-    print(cmd)
 
     add_after = await nym_helper(pool_handle, wallet_handle, trustee_did, another_random_did)
-    time.sleep(1)
+    assert add_after['op'] == 'REPLY'
+    time.sleep(2)
     get_after = await get_nym_helper(pool_handle, wallet_handle, trustee_did, another_random_did)
+    assert get_after['result']['seqNo'] is not None
 
     print(add_after, '\n', get_after)
 
+    req = await ledger.build_get_validator_info_request(trustee_did)
     results = json.loads(await ledger.sign_and_submit_request(pool_handle, wallet_handle, trustee_did, req))
     result = json.loads(results['Node4'])
     primary_after =\
         result['result']['data']['Node_info']['Replicas_status']['Node4:0']['Primary'][len('Node'):-len(':0')]
+    host.run('systemctl start indy-node')
 
-    assert add_before['op'] == 'REPLY'
-    assert get_before['result']['seqNo'] is not None
-    assert cmd.exit_status == 0
-    assert add_after['op'] == 'REPLY'
-    assert get_after['result']['seqNo'] is not None
     assert primary_before != primary_after
 
     print(primary_before)
@@ -57,10 +51,8 @@ async def test_vc_by_restart():
 
 
 @pytest.mark.asyncio
-async def test_vc_by_demotion():
-    await pool.set_protocol_version(2)
-    pool_handle, _ = await pool_helper()
-    wallet_handle, _, _ = await wallet_helper()
+async def test_vc_by_demotion(looper, simple):
+    pool_handle, wallet_handle = simple
     trustee_did, trustee_vk = await did.create_and_store_my_did(wallet_handle, json.dumps(
         {'seed': '000000000000000000000000Trustee1'}))
     random_did = random_did_and_json()[0]

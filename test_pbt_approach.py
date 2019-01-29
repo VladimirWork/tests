7986c8cf8613a76fec0a51ef70a389eeec3a93bb
Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>
2019-01-29 19:15:22 +0300

    update

--- test_pbt_approach.py
+++ test_pbt_approach.py
@@ -1,11 +1,16 @@
 import pytest
 from hypothesis import given
-from hypothesis.strategies import text, characters, decimals
+from hypothesis.strategies import text, characters, decimals, composite
 from indy import *
 import json
 
 
-@given(text().map(lambda x: x + 'text'))
+@composite
+def some_strategy(draw):
+    return draw(text().map(lambda x: x+'text'))
+
+
+@given(name=some_strategy())
 @pytest.mark.asyncio
 async def test_build_nym_request(name):
     res = await anoncreds.issuer_create_schema('V4SGRU86Z58d6TV7PBUe6f', name, '1.0',

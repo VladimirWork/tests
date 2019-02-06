e5c100e65312404dc68d2ad1debb48ac9a4007d1
Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>
2019-02-06 16:30:25 +0300

    update

--- test_roles.py
+++ test_roles.py
@@ -3,7 +3,7 @@ from utils import *
 
 
 @pytest.mark.asyncio
-async def test_misc_roles(pool_handler, wallet_handler, get_default_trustee):
+async def test_roles(pool_handler, wallet_handler, get_default_trustee):
     trustee_did, _ = get_default_trustee
     new_steward_did, new_steward_vk = await did.create_and_store_my_did(wallet_handler, '{}')
     anchor1_did, anchor1_vk = await did.create_and_store_my_did(wallet_handler, '{}')
@@ -13,18 +13,23 @@ async def test_misc_roles(pool_handler, wallet_handler, get_default_trustee):
     res1 = await nym_helper(pool_handler, wallet_handler, trustee_did, new_steward_did, new_steward_vk, None,
                             'STEWARD')
     assert res1['op'] == 'REPLY'
-
     # New Steward adds TA1
     res2 = await nym_helper(pool_handler, wallet_handler, new_steward_did, anchor1_did, anchor1_vk, None,
                             'TRUST_ANCHOR')
     assert res2['op'] == 'REPLY'
-
-    # Trustee removes new Steward
+    # Trustee demotes new Steward
     res3 = await nym_helper(pool_handler, wallet_handler, trustee_did, new_steward_did, None, None,
                             '')
     assert res3['op'] == 'REPLY'
-
-    # New Steward try to add TA2
+    # New Steward try to add TA2 after demotion - should fail
     res4 = await nym_helper(pool_handler, wallet_handler, new_steward_did, anchor2_did, anchor2_vk, None,
                             'TRUST_ANCHOR')
     assert res4['op'] == 'REJECT'
+    # Trustee promotes new Steward back
+    res5 = await nym_helper(pool_handler, wallet_handler, trustee_did, new_steward_did, None, None,
+                            'STEWARD')
+    assert res5['op'] == 'REPLY'
+    # New Steward try to add TA2 after promotion
+    res6 = await nym_helper(pool_handler, wallet_handler, new_steward_did, anchor2_did, anchor2_vk, None,
+                            'TRUST_ANCHOR')
+    assert res6['op'] == 'REPLY'

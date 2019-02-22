f4adfb7a9d4a3145a52a400ef85aaff2a564f1e7
Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>
2019-02-22 12:58:52 +0300

    update

--- indy-sdk-tests/test_vcx_acceptance.py
+++ indy-sdk-tests/test_vcx_acceptance.py
@@ -1,34 +1,42 @@
 import pytest
+import json
+import time
+import os
 from utils import payment_initializer
-from vcx.api.vcx_init import vcx_init
+from vcx.api.vcx_init import vcx_init_with_config
+from vcx.api.utils import vcx_agent_provision
 from vcx.api.wallet import Wallet
-from vcx.common import mint_tokens
+from vcx.common import mint_tokens, shutdown
 
 # import logging
 # logging.basicConfig(level=0)
-# logging.basicConfig( level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 # logging.getLogger().setLevel(0)
 
 
 @pytest.mark.asyncio
 async def test_vcx_mint_token():
-    config = './config.json'
     library = 'libnullpay.so'
     initializer = 'nullpay_init'
+    config = json.dumps(json.loads(open('./config.json').read()))
     await payment_initializer(library, initializer)
-
+    os.system('cd /home/indy/indy-sdk/vcx/dummy-cloud-agent; cargo run sample-config.json &')
+    time.sleep(15)
     """ Mint tokens to send """
     # Create the connection to before processing the credential
-    await vcx_init(config)
-
+    await vcx_agent_provision(config)
+    await vcx_init_with_config(config)
     # Mint tokens and store in wallet
     mint_tokens()  # three addresses and 1000 tokens each - puts stuff in wallet only...
-
     tkn = await Wallet.get_token_info(0)
-    print("Token info %s " % tkn)
+    print("\nToken info before: %s " % tkn)
     address = await Wallet.create_payment_address()
     # Send tokens - test for EN-479
-    rec = await Wallet.send_tokens(0, 50000000000, address.decode('utf-8'))
+    rec = await Wallet.send_tokens(0, 50000000000, address.decode())
+    rec = json.loads(rec.decode().strip())
+    assert rec['op'] == 'REPLY'
     tkn2 = await Wallet.get_token_info(0)
-    print("============")
-    print(tkn2)
+    print("\nToken info after: %s " % tkn2)
+    assert tkn != tkn2
+    shutdown(True)
+    os.system('killall -9 indy-dummy-agent')
